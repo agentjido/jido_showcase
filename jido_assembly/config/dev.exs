f709d78698c88c14c4802e6351fa3c2b10f06a7c
Mike Hostetler <84222+mikehostetler@users.noreply.github.com>
2026-08-07 10:08:44 -0500

    fix(ci): validate development runtime

--- jido_assembly/config/dev.exs
+++ jido_assembly/config/dev.exs
@@ -48,11 +48,11 @@ config :jido_assembly, Jido.AssemblyWeb.Endpoint,
     web_console_logger: true,
     patterns: [
       # Static assets, except user uploads
-      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
+      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$",
       # Router, Controllers, LiveViews and LiveComponents
-      ~r"app/.*\.(ex|holo)$"E,
-      ~r"lib/jido_assembly_web/router\.ex$"E,
-      ~r"lib/jido_assembly_web/(controllers|live|components)/.*\.(ex|heex)$"E
+      ~r"app/.*\.(ex|holo)$",
+      ~r"lib/jido_assembly_web/router\.ex$",
+      ~r"lib/jido_assembly_web/(controllers|live|components)/.*\.(ex|heex)$"
     ]
   ]
 
